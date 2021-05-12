9ce61acaaf059adaec5b3e3aa107c83990f969d2
Mateusz <74718131+MateuszBilik@users.noreply.github.com>
2021-05-13 00:49:53 +0200

    print tabele fixed

--- src/main/java/api/Pollution.java
+++ src/main/java/api/Pollution.java
@@ -1,8 +1,9 @@
 package api;
 
-import lombok.*;
-
-import java.math.BigDecimal;
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
 
 @Getter
 @Setter
